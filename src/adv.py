1915a74055c9ee849ab612b5146f7031121d0818
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-02-12 15:46:16 -0800

    Finished mvp 1

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -33,14 +34,94 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+
+
+
+
 #
 # Main
 #
+#______________________
+#_____|__ok___||___t___
+#_____|___f____|___n___
+#_____|___os___|_______
 
 # Make a new player object that is currently in the 'outside' room.
 
+jordan = Player('outside')
+print(jordan.inventory)
+
+# 0 = north(n), 1 = east(e), 2 = south(s) , 3 = west(w)
+
+movements = ['n', 'e', 's', 'w']
+leave_game = 'q'
+
 # Write a loop that:
 #
+#if an if is nested in anotehr if the first if is checked and the second if is checked
+#if an if condition is met, the elif condition will not run
+
+x = True
+while x:
+    player_move = input("~~> ")
+    if player_move == leave_game:
+        print('Goodbye!!!')
+        break
+    if jordan.player_room is 'outside':
+      if player_move == movements[0]:
+          jordan.player_room = 'foyer'
+          print(f"{room['outside'].n_to.name}, \n {room['outside'].n_to.description}")
+      else:
+          print('Sorry, you can not go that way!')
+    elif jordan.player_room is 'foyer':
+        if player_move == str(movements[2]):
+            print(f"{room['foyer'].s_to.name}, \n {room['foyer'].s_to.description}")
+            jordan.player_room = 'outside'
+        elif player_move == str(movements[0]):
+            print(f"{room['foyer'].n_to.name}, \n {room['foyer'].n_to.description}")
+            jordan.player_room = 'overlook'
+        elif player_move == str(movements[1]):
+            print(f"{room['foyer'].e_to.name}, \n {room['foyer'].e_to.description}")
+            jordan.player_room = 'narrow'
+        elif player_move == str(movements[3]):
+            print('Sorry, you can not go that way!')
+    elif jordan.player_room is 'overlook':
+        if player_move == str(movements[2]):
+            print(f"{room['overlook'].s_to.name}, \n {room['overlook'].s_to.description}")
+            jordan.player_room = 'foyer'
+        else:
+            print('Sorry, you can not go that way!')
+    elif jordan.player_room is 'narrow':
+        if player_move == str(movements[0]):
+            print(f"{room['narrow'].n_to.name}, \n {room['narrow'].n_to.description}")
+            jordan.player_room = 'treasure'
+        elif player_move == str(movements[3]):
+            print(f"{room['narrow'].w_to.name}, \n {room['narrow'].w_to.description}")
+            jordan.player_room = 'foyer'    
+        else:
+            print("sorry you can not go that way!")    
+    elif jordan.player_room is 'treasure':
+        if player_move == str(movements[2]):
+            print(f"{room['treasure'].s_to.name}, \n {room['treasure'].s_to.description}")
+            jordan.player_room = 'narrow'
+        else:
+            print('you can not go that way!')                
+
+            
+
+    
+
+    
+
+
+
+
+
+
+   
+    
+
+    
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+  def __init__(self, player_room, inventory=[]):
+    self.player_room = player_room
+    self.inventory = inventory
+  def __str__(self):
+    return (f"{self.player_room} ")  
+
